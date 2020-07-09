177f90680a339173fe3b03b5c8b975c099498075
Cperez354 <cperez354@gmail.com>
2020-07-08 22:31:03 -0500

    We already finish three courses.

---
+++ 02-JsBasic/valores.js
@@ -0,0 +1,13 @@
+// valores Primitivos
+40
+"Cristian Camilo Perez"
+// Valores booleanos
+true
+false
+// Valores placeholder
+null --> typeof object
+undefined
+
+// Valores no primitivos
+[1,2,3]
+{ nombre: 'Diego' }
---
+++ 02-JsBasic/variables.js
@@ -0,0 +1,21 @@
+// Reservar espacio de memoria
+var nombre = "Oscar";
+
+// Estados de una variable
+// Declarar
+var edad;
+// Inicializar
+edad = 23;
+
+edad
+
+// Inicializar un arreglo
+var elementos = ["Computador", "Celular"];
+
+// Tipo objeto
+var persona = {
+    nombre: "diego",
+    edad: 30
+};
+
+persona
